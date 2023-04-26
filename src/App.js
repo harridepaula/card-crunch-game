c236f96ea1e92b00066bbc115aea9d3e47982336
Harri <harri.kinsman@gmail.com>
2023-04-26 17:17:55 +0100

    Creating react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+
+const cardImages = [
+    { "src": "/img/helmet-1.png" },
+    { "src": "/img/potion-1.png" },
+    { "src": "/img/ring-1.png" },
+    { "src": "/img/scroll-1.png" },
+    { "src": "/img/shield-1.png" },
+    { "src": "/img/sword-1.png" },
+]
+
+function App() {
+
+    // shuffle cards
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages]
+    }
+
+    return (
+        <div className="App">
+            <h1>Card Crunch</h1>
+            <button>New Game</button>
+        </div>
+    );
+}
+
+export default App;
