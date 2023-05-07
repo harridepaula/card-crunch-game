aeb5bafe8f803b8c76ec0495dc92620d57ceaa00
Harri <harri.kinsman@gmail.com>
2023-05-07 19:42:40 +0100

    Styling flipped cards rotation

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ function App() {
                         key={card.id}
                         card={card}
                         handleChoice={handleChoice}
+                        flipped={card === choiceOne || card === choiceTwo || card.matched}
                     />
                 ))}
             </div>
